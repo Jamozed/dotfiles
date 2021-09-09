491d1c5d3ab4fde8dbcab6fc6a56727d262d4dcc
Jamozed <jake@omkov.net>
2021-09-09 20:30:34 +1200

    Add Git config file

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ DIR="$(dirname $(realpath "$0"))"
 # Check XDG environment variables, and set the to default if unset
 if [ -z "$XDG_CONFIG_HOME" ]; then XDG_CONFIG_HOME="$HOME/.config"; fi
 
+mkdir -p "$XDG_CONFIG_HOME/git" && ln -s "$DIR/.config/git/config" "$XDG_CONFIG_HOME/git/config"
 mkdir -p "$XDG_CONFIG_HOME/nvim" && ln -s "$DIR/.config/nvim/init.lua" "$XDG_CONFIG_HOME/nvim/init.lua"
