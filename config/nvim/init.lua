8da89efac4f182590ea51de4eab4c42d19f33ffd
Jamozed <jake@omkov.net>
2022-03-26 00:54:40 +1300

    Add gruvbox theme to Neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,28 +1,41 @@
--- init.lua
 -- Config file for Neovim
 -- Located in ~/.config/nvim
 
-vim.opt.clipboard      = "unnamedplus" -- Enable system clipboard support
-vim.opt.completeopt    = "menuone,noinsert,noselect"
-vim.opt.cpoptions      = "I"           -- Do not delete autoindents
-vim.opt.foldlevelstart = 99            -- Begin with all folds open
-vim.opt.foldmethod     = "syntax"      -- Use syntax highlighting for folds
-vim.opt.history        = 1000          -- Remember 1000 lines of history
-vim.opt.ignorecase     = true          -- Ignore case in search patterns
-vim.opt.mouse          = "a"           -- Enable mouse support
-vim.opt.number         = true          -- Enable line numbers
-vim.opt.shiftwidth     = 4             -- Set autoindent width
-vim.opt.smartcase      = true          -- Don't ignore case with capitals
-vim.opt.smartindent    = true          -- Enable smart indent
-vim.opt.splitbelow     = true          -- Split below the current window
-vim.opt.splitright     = true          -- Split right from the current window
-vim.opt.swapfile       = false         -- Disable swap files
-vim.opt.synmaxcol      = 240           -- Process syntax up to 240 columns
-vim.opt.syntax         = "enable"      -- Enable syntax highlighting
-vim.opt.tabstop        = 4             -- Set tab width
-vim.opt.termguicolors  = true          -- Enable 24-bit colour in the TUI
-vim.opt.undofile       = true          -- Enable undo files
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = "menuone,noinsert,noselect"
+vim.opt.cpoptions = "I" -- Do not delete autoindents
+vim.opt.foldlevelstart = 99
+vim.opt.foldmethod = "syntax"
+vim.opt.history = 1000
+vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.synmaxcol = 240 -- Process syntax up to 240 columns
+vim.opt.syntax = "enable"
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.undofile = true
 
-require "paq" { "hoob3rt/lualine.nvim", "savq/paq-nvim" }
+require("paq") {
+	"hoob3rt/lualine.nvim",
+	"morhetz/gruvbox",
+	"nvim-lua/plenary.nvim",
+	"nvim-telescope/telescope.nvim",
+	"savq/paq-nvim",
+}
 
-require("lualine").setup { options = { icons_enabled = false } }
+require("lualine").setup({ options = { icons_enabled = false }})
+
+vim.g["gruvbox_contrast_dark"] = "hard"
+vim.cmd("autocmd vimenter * ++nested colorscheme gruvbox")
+
+vim.cmd("nnoremap <Leader>ff <Cmd>Telescope find_files<CR>")
+vim.cmd("nnoremap <Leader>fg <Cmd>Telescope live_grep<CR>")
+vim.cmd("nnoremap <Leader>fb <Cmd>Telescope buffers<CR>")
+vim.cmd("nnoremap <Leader>fh <Cmd>Telescope help_tags<CR>")
